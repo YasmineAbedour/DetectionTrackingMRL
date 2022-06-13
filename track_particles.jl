9cee80f420b35ea298d0da48ba35fd9f5d2246c9
Yasmine Abedour <74671467+YasmineAbedour@users.noreply.github.com>
2022-06-13 16:41:07 +0200

    Add files via upload

---
+++ track_particles.jl
@@ -0,0 +1,46 @@
+using BlobTracking, Images, VideoIO, ImageView
+using CSV
+using DataFrames
+include("save_data.jl")
+
+
+#opens the video, creates a iterable stack of frames stored in "vid"
+path = "C:\\Users\\Yasmine\\SANDBOX\\git files\\pontedera\\images_semaine1\\h2o2_1_whitouttag.mp4"
+io   = VideoIO.open(path)
+vid  = VideoIO.openvideo(io)
+img  = first(vid)
+
+#creates a blob tracker with the desired parameters.
+bt = BlobTracker(5:11, #array of blob sizes we want to detect 
+                3.0, # σw Dynamics noise std. (kalman filter param)
+                10.0,  # σe Measurement noise std. (pixels) (kalman filter param)
+                #mask=mask, #image processing before the detection, not implemented here because unecessary
+                #preprocessor = preprocessor, #image processing before the detection, not implemented here because unecessary
+                amplitude_th = 0.008, ## with less, like 0.007, it detects false positives (in the Hirox videos)
+                correspondence = HungarianCorrespondence(p=0.5, dist_th=4), # dist_th is the number of sigmas away from a predicted location a measurement is accepted.
+)
+
+#tune_size can be used to automatically tune the size array in bt based on img (the first img of vid). not mandatory.
+#tune_sizes(bt, img)
+
+
+result = track_blobs(bt, vid,
+                        display = Base.display, # use nothing to omit displaying.
+                        recorder = Recorder(),) # records result to video on disk
+
+
+#plots trajectories and start-end points for each blob
+traces = trace(result, minlife=15) # Filter minimum lifetime of 5
+measurement_traces = tracem(result, minlife=5)
+drawimg = RGB.(img)
+draw!(drawimg, traces, c=RGB(0,0,0.5))
+draw!(drawimg, measurement_traces, c=RGB(0.5,0,0))
+
+#if we just need the coordinates whitout tracking, use this
+coords = get_coordinates(bt, vid)
+
+
+#saves data in a dataframe in .csv file. 4 columns: blob ID, time, x and y for each frame.
+#framerate is the frame rate of the video
+save_data(result,framerate)
+
---
+++ track_particles_live.jl
@@ -0,0 +1,57 @@
+using BlobTracking, Images, VideoIO, ImageView
+import BlobTracking.track_blobs
+
+cam = VideoIO.opencamera()
+
+img = read(cam)
+
+bt = BlobTracker(5:6, #sizes 
+                #2.0, # σw Dynamics noise std.
+                3.0,
+                10.0,  # σe Measurement noise std. (pixels)
+                #mask=mask,
+                #preprocessor = preprocessor,
+                amplitude_th = 0.008, ## a 0.007 ca detecte des faux positifs 
+                correspondence = HungarianCorrespondence(p=0.5, dist_th=4), # dist_th is the number of sigmas away from a predicted location a measurement is accepted.
+)
+tune_sizes(bt, img)
+
+
+function BlobTracking.track_blobs(bt::BlobTracker, vid, nbframes::Int; display=nothing, recorder=nothing, threads=Threads.nthreads()>1, ignoreempty=false)
+    result = TrackingResult()
+    #buffer = threads ? coordinate_iterator(bt, vid) : vid
+    buffer=vid
+    img,buffer = Iterators.peel(buffer)
+    ws = BlobTracking.Workspace(img, length(bt.sizes))
+    img, coord_or_img = img isa Tuple ? img : (img,img)
+    measurement = Measurement(ws, bt, coord_or_img, result)
+    BlobTracking.spawn_blobs!(result, bt, measurement)
+    BlobTracking.showblobs(RGB.(Gray.(img)), result, measurement, recorder = recorder, display=display)
+
+    try
+        for (ind,img) in enumerate(Base.Iterators.take(buffer, nbframes))
+            println("Frame $ind")
+            img, coord_or_img = img isa Tuple ? img : (img,img)
+            measurement = update!(ws, bt, coord_or_img , result)
+            BlobTracking.showblobs(RGB.(Gray.(img)),result,measurement, rad=6, recorder=recorder, display=display, ignoreempty=ignoreempty)
+        end
+    finally
+        finalize(recorder)
+    end
+    result
+end
+
+nbframes = 15
+result = track_blobs(bt, vid,nbframes,
+                            display = nothing, #Base.display, # use nothing to omit displaying.
+                            recorder=nothing,) # records result to video on disk
+
+close(cam)
+
+traces = trace(result, minlife=5) # Filter minimum lifetime of 5
+measurement_traces = tracem(result, minlife=5)
+drawimg = RGB.(img)
+draw!(drawimg, traces, c=RGB(0,0,0.5))
+draw!(drawimg, measurement_traces, c=RGB(0.5,0,0))
+
+coords = get_coordinates(bt, cam)
