d2a2ac4c755592ec876a817f27bac4cc824c7dcb
Yasmine Abedour <74671467+YasmineAbedour@users.noreply.github.com>
2022-06-30 10:19:26 +0200

    Update detect_circles.jl

--- detect_circles.jl
+++ detect_circles.jl
@@ -14,7 +14,7 @@ function detect_circles(path)
 
     circle_centers, circle_radius = hough_circle_gradient(bool_img_edges, img_phase, 8:10) 
 
-    ##ajoute boucle for pour quil arrete de compter les centres hors de limage 
+ 
     img_demo = Float64.(bool_img_edges); for c in circle_centers img_demo[c] = 2; end
     imshow(img_demo)
 
