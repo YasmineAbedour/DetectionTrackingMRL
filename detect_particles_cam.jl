c0e12d659cd62291b637f407683f105df647435e
Yasmine Abedour <74671467+YasmineAbedour@users.noreply.github.com>
2022-07-09 16:47:54 +0200

    Update detect_particles_cam.jl

--- detect_particles_cam.jl
+++ detect_particles_cam.jl
@@ -8,20 +8,18 @@ using Plots;
 
 cam = VideoIO.opencamera()
 img = read(cam)
-close(cam)
+
 bt = BlobTracker(5:10, #sizes 
-                #2.0, # σw Dynamics noise std.
-                3.0,
+                3.0, # σw Dynamics noise std.
                 10.0,  # σe Measurement noise std. (pixels)
                 #mask=mask,
                 #preprocessor = preprocessor,
-                amplitude_th = 0.008, ## a 0.007 ca detecte des faux positifs 
+                amplitude_th = 0.008,  
                 correspondence = HungarianCorrespondence(p=0.5, dist_th=4), # dist_th is the number of sigmas away from a predicted location a measurement is accepted.
 )
 tune_sizes(bt, img)
 
 ws = BlobTracking.Workspace(img, length(bt.sizes))
-
 coordos = Trace[]
 
 function detection_frame(bt::BlobTracker, coordos, ws; threads=Threads.nthreads()>1,)
